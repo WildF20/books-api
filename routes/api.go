cd36f7152a10eb4a1bdd1afcc8544dcd7e78e582
WildF20 <wildanfebrian9@gmail.com>
2025-05-27 18:39:36 +0700

    update

--- routes/api.go
+++ routes/api.go
@@ -4,13 +4,10 @@ import (
 	"books-api/handler"
 
 	"github.com/go-chi/chi/v5"
-    "github.com/go-chi/chi/v5/middleware"
 )
 
 func BookRoutes() func(r chi.Router) {
 	return func(r chi.Router) {
-		r.Use(middleware.Logger)
-
 		r.Get("/", handler.GetBooks)
 		r.Get("/{id}", handler.GetBookByID)
 		r.Post("/", handler.CreateBook)
